824354dc3a82dad73d53d57e20c9271c31ad68be
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2020-02-15 20:38:43 -0500

    add another approach to the last problem

--- practice/removeNthfromEndOfLinkedlis.js
+++ practice/removeNthfromEndOfLinkedlis.js
@@ -64,3 +64,26 @@ function removeNthFromEnd(head, n) {
     return first;
   }
 }
+
+// Cleaner version having three pointers
+
+var removeNthFromEnd = function(head, n) {
+  let dummy = new ListNode(0);
+  dummy.next = head;
+
+  let first = dummy;
+  let second = dummy;
+
+  for (let i = 0; i <= n; i++) {
+    first = first.next;
+  }
+
+  while (first) {
+    first = first.next;
+    second = second.next;
+  }
+
+  second.next = second.next.next;
+
+  return dummy.next;
+};
